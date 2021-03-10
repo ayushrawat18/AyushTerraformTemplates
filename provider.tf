bcf0ff46b13ed99378071818843937fb77316319
Ayush Rawat ITOM <ayushrawat18@gmail.com>
2021-03-10 17:09:26 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,12 @@
 provider "azurerm" {
   features {}
   subscription_id = var.subscriptionId
-  /*
+  
   subscription_id = "${var.subscriptionId}"  
   client_id       = "${var.clientId}"
   client_secret   = "${var.clientSecret}"
   tenant_id       = "${var.tenantId}"
-  */
-  client_id = var.client_id
-  client_secret = var.client_secret
-  tenant_id = var.tenant_id  
+  
   
 }
 
