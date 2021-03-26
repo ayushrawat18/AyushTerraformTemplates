3b49a524dbfdd0cdd0444a0ec0fb2cf397fd46d8
Ayush Rawat ITOM <ayushrawat18@gmail.com>
2021-03-26 11:21:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,197 +1,180 @@
+#Directory = /vctran62/azure-vm-customtags::master
+#File1 = metadata.snc
+#File2 = provider.tf
+#File3 = template.tf
+#File4 = var.tf
 
-#Directory = /jagadt80/tf-az-poc::main
-#File1 = main.tf
-#File2 = outputs.tf
-#File3 = providers.tf
-#File4 = variables.tf
+#File =metadata.snc
+"Metadata": {
+          "SNC::Parameter::Metadata": {
+               "resourceGroup":{
+                     "datasource":"ServiceNow::Pools::ResourceGroupPool.getByLDC"
+                },
+		"vmSize":{
+              	      "datasource":"ServiceNow::Pools::ComputeTemplatePool.getTemplateNameByLDC"
+                }
 
-#File =main.tf
-module "new_subscription" {
-  source = "./modules/subscription"
-}
-
-module "management_groups" {
-  source            = "./modules/management-groups"
-  EA_subscriptionid = module.new_subscription.EA_subscriptionid
-}
-
-module "policy_assignments" {
-  source                      = "./modules/policy-assignments"
-  EA_id                       = module.new_subscription.EA_id
-  tag_governance_policyset_id = module.policyset_definitions.tag_governance_policyset_id
-}
-
-module "policy_definitions" {
-  source = "./modules/policy-definitions"
-  # childmg_id = module.management_groups.childmg_id
-  EA_subscriptionid = module.new_subscription.EA_subscriptionid
-}
-
-module "policyset_definitions" {
-  source = "./modules/policyset-definitions"
-  # childmg_id = module.management_groups.childmg_id
-  EA_subscriptionid = module.new_subscription.EA_subscriptionid
-  custom_policies_tag_governance = [
-    {
-      policyID = module.policy_definitions.addTagToRG_policy_ids[0]
-    },
-    {
-      policyID = module.policy_definitions.addTagToRG_policy_ids[1]
-    },
-    {
-      policyID = module.policy_definitions.addTagToRG_policy_ids[2]
-    },
-    {
-      policyID = module.policy_definitions.addTagToRG_policy_ids[3]
-    },
-    {
-      policyID = module.policy_definitions.addTagToRG_policy_ids[4]
-    },
-    {
-      policyID = module.policy_definitions.addTagToRG_policy_ids[5]
-    },
-    {
-      policyID = module.policy_definitions.inheritTagFromRG_policy_ids[0]
-    },
-    {
-      policyID = module.policy_definitions.inheritTagFromRG_policy_ids[1]
-    },
-    {
-      policyID = module.policy_definitions.inheritTagFromRG_policy_ids[2]
-    },
-    {
-      policyID = module.policy_definitions.inheritTagFromRG_policy_ids[3]
-    },
-    {
-      policyID = module.policy_definitions.inheritTagFromRG_policy_ids[4]
-    },
-    {
-      policyID = module.policy_definitions.inheritTagFromRG_policy_ids[5]
+            }
     }
-  ]
-}
 
-#File =outputs.tf
-output "child_management_group" {
-  description = "The id of the newly created Child Azure management group"
-  value       = module.management_groups.childmg_id
-}
-
-output "parent_management_group" {
-  description = "The id of the newly created Parent Azure management group"
-  value       = module.management_groups.parentmg_id
-}
-
-output "new_subscription" {
-  description = "The subscription id of the newly created Azure EA subscription"
-  value       = module.new_subscription.EA_subscriptionid
-}
-
-output "new_subscription_owners" {
-  description = "The subscription owners of the newly created Azure EA subscription"
-  value       = module.new_subscription.EA_owners
-}
-
-
-
-#File =providers.tf
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-    azure-preview = {
-      source  = "TeckResourcesTDS/azure-preview"
-      version = "0.1.3"
-    }
-  }
-}
-
+#File =provider.tf
 provider "azurerm" {
-  features {}
+  
   subscription_id = "${var.subscriptionId}"
   client_id       = "${var.clientId}"
   client_secret   = "${var.clientSecret}"
   tenant_id       = "${var.tenantId}"
+
+#  features {}
+   version = "=1.44.0"
 }
 
 
-#File =variables.tf
+#File =template.tf
+
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.resourceGroup}"
+  location = "${var.region}"
+  tags = "${jsondecode(var.customtags)}"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["${var.address_space}"]
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  tags = "${jsondecode(var.customtags)}"
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = "${azurerm_resource_group.main.name}"
+  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  address_prefix       = "${var.subnet_prefix}"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+
+  ip_configuration {
+    name                          = "${var.prefix}-ipconfiguration"
+    subnet_id                     = "${azurerm_subnet.internal.id}"
+    private_ip_address_allocation = "Dynamic"
+  }
+  tags = "${jsondecode(var.customtags)}"
+}
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-azure-vm"
+  location              = "${azurerm_resource_group.main.location}"
+  resource_group_name   = "${azurerm_resource_group.main.name}"
+  network_interface_ids = ["${azurerm_network_interface.main.id}"]
+  vm_size               = "${var.vmSize}"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "${var.image_publisher}"
+    offer     = "${var.image_offer}"
+    sku       = "${var.image_sku}"
+    version   = "${var.image_version}"
+  }
+  storage_os_disk {
+    name              = "${var.prefix}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "${var.hostname}"
+    admin_username = "${var.admin_username}"
+    admin_password = "${var.admin_password}"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+ }
+  tags = "${jsondecode(var.customtags)}" 
+  
+}
+
+
+resource "azurerm_public_ip" "test" {
+  name                = "${var.prefix}-PublicIp"
+  location            = "${var.region}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  allocation_method   = "Static"
+
+  tags = "${jsondecode(var.customtags)}" 
+  
+}
+
+
+#File =var.tf
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "region" {
+  description = "The Azure Region in which all resources in this example should be created"
+}
+
+
 variable "subscriptionId" {}
 variable "clientId" {}
 variable "clientSecret" {}
+variable "resourceGroup" {}
 variable "tenantId" {}
-
-variable "name" {
-  description = "The display name of the subscription"
-  type        = string
-  default = "clz-wpp-wppit-snowmanagement"
+variable "vmSize" {
+  description = "Specifies the size of the virtual machine."
+  default     = "Standard_D1_v2"
+}
+variable "admin_username" {
+  description = "administrator user name"
+  default     = "vmadmin"
 }
 
-variable "NewParentMGName" {
-  description = "The name of the new parent management group"
-  type        = string
-  default = "WPPShared"
+variable "admin_password" {
+  description = "administrator password (recommended to disable password auth)"
+  default = "admin01!"
+}
+variable "address_space" {
+  description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
+  default     = "10.0.0.0/16"
+}
+variable "subnet_prefix" {
+  description = "The address prefix to use for the subnet."
+  default     = "10.0.10.0/24"
+}
+variable "image_publisher" {
+  description = "name of the publisher of the image (az vm image list)"
+  default     = "Canonical"
 }
 
-variable "NewParentMGDisplayName" {
-  description = "The display name of the new parent management group"
-  type        = string
-  default = "WPP Shared"
+variable "image_offer" {
+  description = "the name of the offer (az vm image list)"
+  default     = "UbuntuServer"
 }
 
-variable "NewChildMGName" {
-  description = "The name of the new child management group"
-  type        = string
-  default = "WPPManagement"
+variable "image_sku" {
+  description = "image sku to apply (az vm image list)"
+  default     = "16.04-LTS"
 }
 
-variable "NewChildMGDisplayName" {
-  description = "The display name of the new child management group"
-  type        = string
-  default = "WPP Management"
+variable "image_version" {
+  description = "version of the image to apply (az vm image list)"
+  default     = "latest"
+}
+variable "hostname" {
+  description = "VM name referenced also in storage-related names."
+  default="tf"
 }
 
-variable "owners" {
-  description = "The list of principals that should be granted Owner access on the subscription. Principals should be of type User, Service Principal or Security Group"
-  type        = list
-  default = ["41237360-560c-4459-91b2-582229a72dd6"]
+variable "customtags" {
 }
-
-variable "offer_type" {
-  description = "The offer type of the subscription. Only valid when creating a subscription in a enrollment account scope. Possible values include: MS-AZR-0017P (production use), MS-AZR-0148P (dev/test)"
-  type        = string
-  default = "MS-AZR-0017P"
-}
-
-variable "enrollment_account" {
-  description = "The name of the enrollment account to which the subscription will be billed"
-  type        = string
-  default = "716560cf-92ba-4082-aafb-0cf5f72c9c6d"
-}
-
-
-variable "mandatory_tag_keys" {
-type = list
-default = [
-    "serviceowner",
-    "cartesiscode",
-    "environment",
-    "opgrp",
-    "opco",
-    "projectcode"
-  ]
-  }
- 
-variable "mandatory_tag_value" {
-type = list
-default = [
-    "NikolaiLakovic",
-    "99999",
-    "propod",
-    "WPP",
-    "WPPIT",
-    "190789"
-  ]
-  }
